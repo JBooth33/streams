6c8f93e6573426b2af62bcda0e0581295c012f63
Joe Booth <joseph.booth3@gmail.com>
2019-03-24 08:57:49 -0500

    adding modal to delete streams

---
+++ client/src/components/Modal.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Modal = () => {
+    return ReactDOM.createPortal(
+        <div className="ui dimmer modals visible active">
+            <div className="ui standard modal visible active">
+                jkfjkfjkfjkfj
+            </div>
+        </div>,
+        document.querySelector('#modal')
+    );
+};
+
+export default Modal;
